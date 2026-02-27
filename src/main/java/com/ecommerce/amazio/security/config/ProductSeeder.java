30479b7c1ffd2a7af1f59fd46ad957011bc684ab
Sunil Reddy <sunilreddymandadi@gmail.com>
2026-02-27 18:37:37 +0530

    Add JSON product seeder

---
+++ src/main/java/com/ecommerce/amazio/security/config/ProductSeeder.java
@@ -0,0 +1,44 @@
+package com.ecommerce.amazio.security.config;
+
+
+import com.ecommerce.amazio.model.Product;
+import com.ecommerce.amazio.repository.ProductRepo;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+
+import java.io.InputStream;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Configuration
+public class ProductSeeder {
+
+    @Bean
+    CommandLineRunner seedProducts(ProductRepo productRepository) {
+        return args -> {
+
+            if (productRepository.count() == 0) {
+
+                ObjectMapper mapper = new ObjectMapper();
+                InputStream inputStream =
+                        new ClassPathResource("products.json").getInputStream();
+
+                List<Product> products =
+                        mapper.readValue(inputStream, new TypeReference<List<Product>>() {});
+
+                for (Product product : products) {
+                    product.setCreatedAt(LocalDateTime.now());
+                    product.setUpdatedAt(LocalDateTime.now());
+                }
+
+                productRepository.saveAll(products);
+
+                System.out.println("🔥 All products seeded successfully.");
+            }
+        };
+    }
+}

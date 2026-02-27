cffe9375d3812188130761cc67bd593399508fa8
Sunil Reddy <sunilreddymandadi@gmail.com>
2026-02-27 18:04:29 +0530

    added vercel origin

--- src/main/java/com/ecommerce/amazio/security/config/SecurityConfig.java
+++ src/main/java/com/ecommerce/amazio/security/config/SecurityConfig.java
@@ -53,7 +53,7 @@ public class SecurityConfig {
 
         CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.setAllowedOrigins(List.of("http://localhost:5173"));
+        configuration.setAllowedOrigins(List.of("http://localhost:5173","https://amazio-frontend.vercel.app"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("Authorization", "Content-Type"));
         configuration.setAllowCredentials(true);
